b3c5ad8f962f7085d8eeb0d44223135214ff6633
Anastasia Kabanovsky <124449724+keirialaa@users.noreply.github.com>
2026-03-05 10:38:23 +0100

    Stop tracking ignored files (.vscode and translate.py)

--- translate.py
+++
@@ -1,1 +0,0 @@
-translate = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat", "mucca": "cow", "pecora": "sheep", "scoiattolo": "squirrel", "ragno": "spider"}
